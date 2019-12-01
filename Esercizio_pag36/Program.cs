8238d7ddaa26b111c78e814c84e762b164e705ce
Matteo Sorrentino <Ciro & Luisa@DESKTOP-1GLGU9U>
2019-12-01 15:19:07 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio_pag36/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_pag36
+{
+    class Program
+    {
+        public static int x;
+
+        static void Main(string[] args)
+        {
+
+            var risultato1 = Task.Factory.StartNew(LetturaX);
+
+            Console.WriteLine(risultato1);
+
+            Console.ReadLine();
+
+        }
+
+        private static int LetturaX()
+        {
+            return x = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Esercizio_pag37/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Esercizio_pag37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var risultato1 = Task.Factory.StartNew(Operazione1);
+
+            var risultato2 = Task.Factory.StartNew(Operazione2);
+
+            var risultato3 = Task.Factory.StartNew(Operazione3);
+
+            Task.WaitAll(risultato2, risultato3);
+
+            var risultato4 = risultato2.Result * risultato3.Result;
+
+            var risultato5 = risultato4 + risultato1.Result;
+
+            Console.WriteLine(risultato5);
+
+            Console.ReadLine();
+
+        }
+
+        private static int Operazione3()
+        {
+            return 5 - 2;
+        }
+
+        private static int Operazione2()
+        {
+            return 1 + 4;
+        }
+
+        private static int Operazione1()
+        {
+            return 2 * 6;
+        }
+    }
+}
